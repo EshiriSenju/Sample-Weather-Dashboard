5fde18f7bd01dd215db8fdc332397b50a011a0e8
EshiriSenju <sahversheri@gmail.com>
2023-12-12 13:47:16 +0100

    Furturistic GUI ver1

--- weather.py
+++ weather.py
@@ -1,50 +1,120 @@
 import sys
 import requests
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QMessageBox
+from datetime import datetime
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel, QLineEdit, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QPalette, QColor, QFontDatabase
 
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.load_fonts()  # Load the custom font
         self.initUI()
 
-    def initUI(self):
-        self.layout = QVBoxLayout()
+    def load_fonts(self):
+        # Load the custom font
+        QFontDatabase.addApplicationFont("Orbitron-SemiBold.ttf")
 
+    def initUI(self):
+        self.layout = QGridLayout()
+
+        # Set the color palette for a dark theme
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        self.setPalette(palette)
+
+        # Styling
+        self.setStyleSheet("""
+            QWidget {
+                font-family: 'Orbitron'; 
+                font-size: 16px;
+                color: #00FF00; 
+                background-color: #1C1C1C;
+            }
+            QLabel, QLineEdit, QPushButton {
+                border-style: solid;
+                border-width: 2px;
+                border-color: #008080;
+                border-radius: 8px;
+                padding: 6px;
+            }
+            QPushButton {
+                color: #00FF00;
+                background-color: #333333;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #444444;
+            }
+            QLineEdit {
+                color: #00FF00;
+                background-color: #333333;
+                font-weight: bold;
+            }
+        """)
+
+        # Widgets
         self.city_entry = QLineEdit(self)
-        self.layout.addWidget(self.city_entry)
+        self.layout.addWidget(self.city_entry, 0, 0, 1, 2)  # Span two columns
 
         self.get_weather_button = QPushButton('Get Weather', self)
         self.get_weather_button.clicked.connect(self.fetch_weather)
-        self.layout.addWidget(self.get_weather_button)
+        self.layout.addWidget(self.get_weather_button, 0, 2)
 
         self.weather_label = QLabel('Weather: ')
-        self.layout.addWidget(self.weather_label)
+        self.layout.addWidget(self.weather_label, 1, 0, 1, 3)  # Span three columns
 
+        self.temperature_label = QLabel('Temperature: ')
+        self.layout.addWidget(self.temperature_label, 2, 0)
+
+        self.humidity_label = QLabel('Humidity: ')
+        self.layout.addWidget(self.humidity_label, 2, 1)
+
+        self.wind_speed_label = QLabel('Wind Speed: ')
+        self.layout.addWidget(self.wind_speed_label, 2, 2)
+
+        self.pressure_label = QLabel('Pressure: ')
+        self.layout.addWidget(self.pressure_label, 3, 0)
+
+        self.sunrise_label = QLabel('Sunrise: ')
+        self.layout.addWidget(self.sunrise_label, 3, 1)
+
+        self.sunset_label = QLabel('Sunset: ')
+        self.layout.addWidget(self.sunset_label, 3, 2)
+
+        # Set main window properties
         self.setLayout(self.layout)
-        self.setWindowTitle('Weather Dashboard')
+        self.setWindowTitle('Futuristic Weather Dashboard')
+        self.setGeometry(300, 300, 600, 400)  # Window size
 
     def fetch_weather(self):
         city = self.city_entry.text()
-        api_key = os.getenv('OPENWEATHER_API_KEY')  # Make sure to set this environment variable
+        api_key = os.getenv('OPENWEATHER_API_KEY')
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-        
+
         try:
             response = requests.get(url)
             response.raise_for_status()
             weather_data = response.json()
-            self.weather_label.setText(f"Weather in {city}: {weather_data['weather'][0]['description'].capitalize()}, Temperature: {weather_data['main']['temp']}°C")
+
+            self.weather_label.setText(f"Weather: {weather_data['weather'][0]['description'].capitalize()}")
+            self.temperature_label.setText(f"Temperature: {weather_data['main']['temp']} °C")
+            self.humidity_label.setText(f"Humidity: {weather_data['main']['humidity']}%")
+            self.wind_speed_label.setText(f"Wind Speed: {weather_data['wind']['speed']} m/s")
+            self.pressure_label.setText(f"Pressure: {weather_data['main']['pressure']} hPa")
+            sunrise_time = datetime.fromtimestamp(weather_data['sys']['sunrise']).strftime('%H:%M')
+            sunset_time = datetime.fromtimestamp(weather_data['sys']['sunset']).strftime('%H:%M')
+            self.sunrise_label.setText(f"Sunrise: {sunrise_time}")
+            self.sunset_label.setText(f"Sunset: {sunset_time}")
         except requests.exceptions.HTTPError as errh:
-            if errh.response.status_code == 404:
-                QMessageBox.warning(self, 'Error', f"Weather data for '{city}' not found. Please check the city name.")
-            else:
-                QMessageBox.warning(self, 'HTTP Error', str(errh))
+            QMessageBox.warning(self, 'Error', f"HTTP Error: {errh}")
         except requests.exceptions.ConnectionError as errc:
-            QMessageBox.warning(self, 'Connection Error', str(errc))
+            QMessageBox.warning(self, 'Error', f"Connection Error: {errc}")
         except requests.exceptions.Timeout as errt:
-            QMessageBox.warning(self, 'Timeout Error', str(errt))
+            QMessageBox.warning(self, 'Error', f"Timeout Error: {errt}")
         except requests.exceptions.RequestException as err:
-            QMessageBox.warning(self, 'Error', str(err))
+            QMessageBox.warning(self, 'Error', f"Error: {err}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
