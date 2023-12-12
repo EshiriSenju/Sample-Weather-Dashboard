c3a91e7616c075384ab57552b7fc0fcc12df6e34
EshiriSenju <sahversheri@gmail.com>
2023-12-12 11:55:24 +0100

    GUI building blocks

--- weather.py
+++ weather.py
@@ -1,46 +1,53 @@
+import sys
 import requests
-import json
-import time
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QMessageBox
 
-def get_weather_data(api_key, city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as errh:
-        if errh.response.status_code == 404:
-            print(f"Weather data fir '{city} not found. Please check the city name.")
-        else :
-            print("Http Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print("Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print("Oops: Something Else", err)
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-def is_valid_city(city_name):
-    return city_name.strip().isalpha()
+    def initUI(self):
+        self.layout = QVBoxLayout()
 
-# variables
-api_key = 'a308ca9a98b6f16cd0621897316cbc14'
+        self.city_entry = QLineEdit(self)
+        self.layout.addWidget(self.city_entry)
 
-while True: #infinte loop
-    city = input("Enter the city name: ")
-    if not is_valid_city(city):
-        print("Invalid city name. Please try agin.")
-        continue
-    for attempt in range(3):
-        weather_data = get_weather_data(api_key, city)
+        self.get_weather_button = QPushButton('Get Weather', self)
+        self.get_weather_button.clicked.connect(self.fetch_weather)
+        self.layout.addWidget(self.get_weather_button)
 
-        if weather_data:
-            print(f"Weather in {city}: {weather_data['weather'][0]['description'].capitalize()}")
-            print(f"Temperature: {weather_data['main']['temp']}°C")
-            print(f"Humidity: {weather_data['main']['humidity']}%")
-        else:
-            print("Failed to retrieve weather data")
-            time.sleep(5)
+        self.weather_label = QLabel('Weather: ')
+        self.layout.addWidget(self.weather_label)
 
-            print("Updating in 19 seconds...")
-            time.sleep(2)
+        self.setLayout(self.layout)
+        self.setWindowTitle('Weather Dashboard')
+
+    def fetch_weather(self):
+        city = self.city_entry.text()
+        api_key = os.getenv('OPENWEATHER_API_KEY')  # Make sure to set this environment variable
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            weather_data = response.json()
+            self.weather_label.setText(f"Weather in {city}: {weather_data['weather'][0]['description'].capitalize()}, Temperature: {weather_data['main']['temp']}°C")
+        except requests.exceptions.HTTPError as errh:
+            if errh.response.status_code == 404:
+                QMessageBox.warning(self, 'Error', f"Weather data for '{city}' not found. Please check the city name.")
+            else:
+                QMessageBox.warning(self, 'HTTP Error', str(errh))
+        except requests.exceptions.ConnectionError as errc:
+            QMessageBox.warning(self, 'Connection Error', str(errc))
+        except requests.exceptions.Timeout as errt:
+            QMessageBox.warning(self, 'Timeout Error', str(errt))
+        except requests.exceptions.RequestException as err:
+            QMessageBox.warning(self, 'Error', str(err))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = WeatherApp()
+    ex.show()
+    sys.exit(app.exec_())
